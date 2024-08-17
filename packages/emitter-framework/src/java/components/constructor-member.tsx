6197450630e1941502bd32993f18610af7a64ba5
M1lney <tomj.milne@gmail.com>
2024-08-17 14:06:08 +1200

    Create component to handle members of constructor. #6

---
+++ packages/emitter-framework/src/java/components/constructor-member.tsx
@@ -0,0 +1,14 @@
+import { ModelProperty, Type } from "@typespec/compiler";
+
+
+export interface ConstructorMemberProps {
+  type: ModelProperty;
+}
+
+export function ConstructorMember({type} : ConstructorMemberProps) {
+  console.log('this.'+ type.name + '=' + type.name)
+  return(
+    <>this.{type.name} = {type.name};
+    </>
+  )
+}
