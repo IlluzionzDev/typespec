b7bcc80ca9bd6dea5abf8dc561477f235f3ce785
M1lney <tomj.milne@gmail.com>
2024-08-17 17:54:15 +1200

    Add constructor with no parameters. #6

--- packages/emitter-framework/src/java/components/model-constructor.tsx
+++ packages/emitter-framework/src/java/components/model-constructor.tsx
@@ -5,25 +5,23 @@ import { mapJoin } from "@alloy-js/core";
 import { ConstructorMember } from "./constructor-member.js";
 
 export interface ModelConstructorProps{
-  properties: ModelProperty[];
+  types: ModelProperty[];
 }
 
 export function ModelConstructor(props : ModelConstructorProps) {
-  const properties = props.properties;
+
+  const properties = props.types;
   const params = createTypeRecord(properties);
   const members = constructorMembersMapJoin(properties);
-  console.log('constructor members:', members);
+
   return<Constructor parameters={params} accessModifier={'public'}>{members}</Constructor>
 }
 
 function createTypeRecord(properties: ModelProperty[]): Record<string, string> {
   const result: Record<string, string> = {};
-  console.log('Properties received by createTypeRecord:', properties);
 
   properties.forEach(property => {
-    const expression = getTypeExpression(property);
-    console.log('Type expression for property', property.name, ':', expression);
-    result[property.name] = expression;
+    result[property.name] = getTypeExpression(property);
   });
 
   return result;
@@ -36,7 +34,6 @@ function getTypeExpression(property: ModelProperty) {
 }
 
 function constructorMembersMapJoin(properties: ModelProperty[]) {
-  console.log('Properties received by constructorMembers:', properties);
 
   return mapJoin(Array.from(properties), (property) => (
     <ConstructorMember type={property}/>
--- packages/emitter-framework/src/java/components/model-declaration.tsx
+++ packages/emitter-framework/src/java/components/model-declaration.tsx
@@ -3,30 +3,35 @@ import { mapJoin } from "@alloy-js/core";
 import { Model} from "@typespec/compiler";
 import { ModelMember } from "./model-member.js";
 import { ModelConstructor } from "./model-constructor.js";
+import { Constructor } from "@alloy-js/java";
 
 export interface ModelDeclarationProps{
   type: Model;
 }
 
 export function ModelDeclaration(props: ModelDeclarationProps) {
-
   const body = getBody(props);
 
   return<jv.Class name={props.type.name}>{body}
   </jv.Class>
 }
 
-function modelConstructor(props: ModelDeclarationProps) {
+
+function modelParameterizedConstructor(props: ModelDeclarationProps) {
   const typeMembers = props.type.properties.values();
-  return<ModelConstructor properties={Array.from(typeMembers)}/>
+
+  return<ModelConstructor types={Array.from(typeMembers)}/>
 }
+
 
 function gettersAndSettersFromType(props: ModelDeclarationProps) {
   const typeMembers = props.type.properties.values();
+
   return mapJoin(Array.from(typeMembers), (member) => (
     <ModelMember type={member} memberGetAndSetMethod={true} />
   ), { joiner: "\n" });
 }
+
 
 function membersFromType(props: ModelDeclarationProps) {
   const typeMembers = props.type.properties.values();
@@ -36,11 +41,14 @@ function membersFromType(props: ModelDeclarationProps) {
   ), { joiner: "\n" });
 }
 
+
 //There may be a cleaner implementation but this gets the job done for now.
 function getBody(props: ModelDeclarationProps) {
   const members = membersFromType(props);
   const gettersAndSetters = gettersAndSettersFromType(props)
-  const constructor = modelConstructor(props);
-  return<>{members}{`\n`}{constructor}{`\n`}{gettersAndSetters}</>
+  const parameterizedConstructor = modelParameterizedConstructor(props);
+  const constructor = <Constructor accessModifier={"public"}></Constructor>
+
+  return<>{members}{`\n`}{constructor}{`\n`}{parameterizedConstructor}{`\n`}{gettersAndSetters}</>
 }
 
